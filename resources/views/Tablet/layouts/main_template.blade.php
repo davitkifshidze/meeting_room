bc0fc81d2837a74d1fd56d9c861c3c901f0f17e7
merabi nadiradze <m.nadiradze@codenile.com>
2023-08-02 13:06:13 +0400

    update meta

--- resources/views/Tablet/layouts/main_template.blade.php
+++ resources/views/Tablet/layouts/main_template.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
     {{-- Font Awesome 6.3.0 --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"/>
